3a873e0acb2597a6637fa30402419b74de8a6e50
Rich Cookson <hellorich@frobitz.com>
2022-10-13 15:17:19 +0100

    Moved Skill component into a subfolder

--- src/components/__tests__/skill.test.js
+++ src/components/__tests__/skill.test.js
@@ -1,7 +1,7 @@
 import React from "react"
 import renderer from "react-test-renderer"
 
-import Skill from "../Skill"
+import Skill from "../Skill/Skill"
 
 describe("Skill", () => {
   it("renders correctly", () => {
