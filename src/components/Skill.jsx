144713d4076d7831eb30ce5bc728703993a00fef
Rich Cookson <hellorich@frobitz.com>
2022-10-13 15:09:46 +0100

    Initial Skill component with Storybook tory and Jest test

---
+++ src/components/Skill.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './skill.css'
+
+export default function Skill ({ skill: { title, level, link } }) {
+  const skillLevel = `skill--${level}`
+  return (
+    <li className="skill-list-item">
+      <a 
+        className={['skill', skillLevel].join(' ')}
+        href="{link}"
+        rel="external"
+      >
+      {title}
+      </a>
+    </li>
+  )
+}
+
+Skill.propTypes = {
+  skill: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+    level: PropTypes.oneOf(['basic', 'advanced']),
+  })
+}
---
+++ src/components/__tests__/skill.test.js
@@ -0,0 +1,23 @@
+import React from "react"
+import renderer from "react-test-renderer"
+
+import Skill from "../Skill"
+
+describe("Skill", () => {
+  it("renders correctly", () => {
+    const args = {
+      skill: {
+        title: 'Test Skill',
+        level: 'advanced',
+        link: 'https://developer.mozilla.org'
+      },
+    }
+    
+    const component = renderer.create(
+      // <Skill id="a" title="Test Skill" url="http://developer.mozilla.org" level="advanced" />
+      <Skill {...args} />
+    )
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
