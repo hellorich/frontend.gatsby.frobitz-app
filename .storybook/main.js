8de01d1b9c9a4f82dba896e7300de7f069f8a5c6
Rich Cookson <hellorich@frobitz.com>
2022-11-29 17:00:24 +0000

    Allowed Storybook to use absolute paths - reconfigured test Component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,28 +1,31 @@
+const path = require('path')
+
 module.exports = {
-  // "stories": [
-  //   "../src/**/*.stories.mdx",
-  //   "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  // ],
-  "stories": ["../src/components/**/*.stories.@(js|jsx)"],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+  'stories': ['../src/components/**/*.stories.@(js|jsx)'],
+  'addons': [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions'
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  'framework': '@storybook/react',
+  'core': {
+    'builder': '@storybook/builder-webpack5'
   },
   webpackFinal: async config => {
+    config.resolve.modules = [
+      path.resolve(__dirname, '../src'),
+      'node_modules'
+    ],
     // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
     config.module.rules[0].exclude = [/node_modules\/(?!(gatsby|gatsby-script)\/)/]
     // Remove core-js to prevent issues with Storybook
     config.module.rules[0].exclude= [/core-js/]
     // Use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
     config.module.rules[0].use[0].options.plugins.push(
-      require.resolve("babel-plugin-remove-graphql-queries")
+      require.resolve('babel-plugin-remove-graphql-queries')
     )
-    config.resolve.mainFields=["browser", "module", "main"]
+    config.resolve.mainFields=['browser', 'module', 'main']
+    
     return config
   },
 }
--- src/components/Skill/Skill.stories.jsx
+++ src/components/Skill/Skill.stories.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Skill from 'src/components/Skill'
+import Skill from 'components/Skill'
 
 export default {
   component: Skill,
